4136d26608f9f9eae92b874f6b0727565c16ab8e
Gabriel Shiu <gabriel@shiu.us>
2022-12-19 22:26:06 -0500

    added intermediary colors; hid minutes if under a minute

--- src/Components/Timer/Timer.jsx
+++ src/Components/Timer/Timer.jsx
@@ -1,135 +1,86 @@
 import React from 'react';
-// import { useState, useEffect } from 'react';
 import "./Timer.css";
 
 export default class Timer extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            // stage: "idle", // idle, prep, running, ending
-            isRunning: false,
+            stage: "idle", // idle, prep, running, ending
             time: 0,
-            intervalId: null
+            intervalId: null,
+            color: "white",
         }
     }
 
     componentDidMount() {
         window.addEventListener("keydown", this.handleKeyDown);
-        // window.addEventListener("keyup", this.handleKeyUp);
+        window.addEventListener("keyup", this.handleKeyUp);
     }
 
     handleKeyDown = (event) => {
-        // color:  #e42a2a;
         if (event.key === " ") {
             if (event.repeat)
                 return;
 
-            // console.log("space key pressed");
-
-            if (this.state.isRunning === false) {
+            if (this.state.stage === "idle") {
                 this.setState({
-                    isRunning: true,
+                    stage: "prep",
                     time: 0,
+                    color: "green",
+                });
+            }
+            else if (this.state.stage === "running") {
+                clearInterval(this.state.intervalId);
+                this.setState({
+                    stage: "ending",
+                    intervalId: null,
+                    color: "maroon",
+                });
+
+            }
+        }
+    }
+
+    handleKeyUp = (event) => {
+        if (event.key === " ") {
+            if (this.state.stage === "prep") {
+                this.setState({
+                    stage: "running",
                     intervalId: setInterval(() => {
                         this.setState({time: this.state.time + 10});
                     }, 10),
+                    color: "white",
                 });
             }
-            else {
-                clearInterval(this.state.intervalId);
+            else if (this.state.stage === "ending") {
                 this.setState({
-                    isRunning: false,
-                    intervalId: null,
+                    stage: "idle",
+                    color: "white",
                 });
             }
         }
     }
 
-    // handleKeyUp = (event) => {
-    //     if (event.key === " ") {
-    //         console.log("space key released");
-    //         if (this.state.isRunning === false) {
-    //             this.setState({
-    //                 isRunning: true,
-    //                 time: 0,
-    //                 intervalId: setInterval(() => {
-    //                     this.setState({time: this.state.time + 10});
-    //                 }, 10),
-    //             });
-    //         }
-    //         else {
-    //             clearInterval(this.state.intervalId);
-    //             this.setState({
-    //                 isRunning: false,
-    //                 intervalId: null,
-    //             });
-    //         }
-    //     }
-    // }
-
     render() {
         const time = this.state.time;
+        const color = this.state.color;
+        // const stage = this.state.stage;
+        
         return (
             <div className="timer">
-                <span className="digits">
-                    {("0" + Math.floor((time / 60000) % 60)).slice(-2)}:
+                <span className="digits" style={{ color: color }}>
+                    {(time > 60000) ? (("0" + Math.floor((time / 60000) % 60)).slice(-2) + ":") : ""}
                 </span>
-                <span className="digits">
+                <span className="digits" style={{ color: color }}>
                     {("0" + Math.floor((time / 1000) % 60)).slice(-2)}.
                 </span>
-                <span className="digits mili-sec">
+                <span className="digits"
+                    style={{ color: color }}
+                    // style={{ color: stage === "prep" || stage === "ending" ? color : "gray" }}
+                >
                     {("0" + ((time / 10) % 100)).slice(-2)}
                 </span>
             </div>
         )
     }
 }
-
-// export default function Timer(props) {
-//     const [isPaused, setIsPaused] = useState(true);
-//     const [time, setTime] = useState(0);
-    
-//     useEffect(() => {
-//         let interval = null;
-
-//         if (isPaused === false) {
-//             interval = setInterval(() => {
-//                 setTime((time) => time + 10);
-//             }, 10);
-//         } else {
-//             clearInterval(interval);
-//         }
-//         return () => {
-//             clearInterval(interval);
-//         };
-//     }, [isPaused]);
-
-//     useEffect(() => {
-//         window.addEventListener("keydown", (event) => {
-//             if (event.key === " ") {
-//                 console.log("space key pressed");
-//                 if (isPaused === false) {
-//                     setIsPaused(true);
-//                 }
-//                 else {
-//                     setIsPaused(false);
-//                     setTime(0);
-//                 }
-//             }
-//         });
-//     }, []);
-
-//     return (
-//         <div className="timer">
-//             <span className="digits">
-//                 {("0" + Math.floor((time / 60000) % 60)).slice(-2)}:
-//             </span>
-//             <span className="digits">
-//                 {("0" + Math.floor((time / 1000) % 60)).slice(-2)}.
-//             </span>
-//             <span className="digits mili-sec">
-//                 {("0" + ((time / 10) % 100)).slice(-2)}
-//             </span>
-//         </div>
-//     );
-// }
