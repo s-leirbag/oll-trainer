a743b987b0610b846b8ab8d3109703b1e6cb2c6c
Gabriel Shiu <gabriel@shiu.us>
2022-12-23 13:33:46 -0500

    turned cases and headers into components

--- src/Components/CaseSelect/CaseSelect.jsx
+++ src/Components/CaseSelect/CaseSelect.jsx
@@ -3,6 +3,31 @@ import "./CaseSelect.css";
 import "../../Constants";
 import { algsGroups, renderGroups, algsInfo } from '../../Constants';
 
+function Case(props) {
+    return (
+        <td
+            className="case"
+            onClick={() => props.onClick()}
+            title={props.name}
+            style={{ background: props.background }}
+        >
+            <img width='100px' src={props.src} alt={props.name}/>
+        </td>
+    );
+}
+
+function GroupHeader(props) {
+    return (
+        <td
+            className="groupHeader"
+            onClick={() => props.onClick()}
+            colSpan={props.colSpan}
+        >
+            {props.name}
+        </td>
+    );
+}
+
 export default class CaseSelect extends React.Component {
     constructor(props) {
         super(props);
@@ -34,56 +59,64 @@ export default class CaseSelect extends React.Component {
 
     toggleAll() {
         this.setState({
-            selected: (
-                this.state.selected.length > 0 ? [] : Object.keys(algsInfo).map(i => parseInt(i))
-            )
+            selected: this.state.selected.length > 0 ? [] : Object.keys(algsInfo).map(i => parseInt(i))
         });
     }
 
     renderCase(i) {
-        let backgroundVal;
-        
-        if (this.state.selected.includes(i))
-            backgroundVal = 'yellow';
-        else
-            backgroundVal = 'white';
+        const bgColor = this.state.selected.includes(i) ? 'yellow' : 'white';
         
         return (
-            <td
-                className="case"
-                // key={i}
-                onClick={() => this.toggleCase(i)}
-                title={algsInfo[i]["name"]}
-                style={{ background: backgroundVal }}
+            <Case
                 key={i}
-            >
-                <img width='100px' src={"pic/" + i + ".svg"} alt={algsInfo[i]["name"]}/>
-            </td>
-        )
+                onClick={() => this.toggleCase(i)}
+                name={algsInfo[i]["name"]}
+                background={bgColor}
+                src={"pic/" + i + ".svg"}
+            />
+        );
+        
+        // return (
+        //     <td
+        //         className="case"
+        //         // key={i}
+        //         onClick={() => this.toggleCase(i)}
+        //         title={algsInfo[i]["name"]}
+        //         style={{ background: bgColor }}
+        //         key={i}
+        //     >
+        //         <img width='100px' src={"pic/" + i + ".svg"} alt={algsInfo[i]["name"]}/>
+        //     </td>
+        // );
     }
 
     renderGroupHeader(group) {
-        const cases = algsGroups[group];
-
         return (
-            <td
+            <GroupHeader
                 key={group}
-                className="groupHeader"
                 onClick={() => this.toggleGroup(group)}
-                colSpan={cases.length}
-            >
-                {group}
-            </td>
-        )
+                colSpan={algsGroups[group].length}
+                name={group}
+            />
+        );
+
+        // return (
+        //     <td
+        //         key={group}
+        //         className="groupHeader"
+        //         onClick={() => this.toggleGroup(group)}
+        //         colSpan={algsGroups[group].length}
+        //     >
+        //         {group}
+        //     </td>
+        // );
     }
 
-    renderGroup(group) {
+    renderGroupCases(group) {
         const cases = algsGroups[group];
-
         const row = [];
-        for (const i of cases) {
+        for (const i of cases)
             row.push(this.renderCase(i));
-        }
 
         return row;
     }
@@ -95,7 +128,7 @@ export default class CaseSelect extends React.Component {
                 {[this.renderGroupHeader(group1), this.renderGroupHeader(group2)]}
             </tr>,
             <tr key={group1 + group2 + "row"}>
-                {[this.renderGroup(group1), this.renderGroup(group2)]}
+                {[this.renderGroupCases(group1), this.renderGroupCases(group2)]}
             </tr>
         ];
     }
@@ -106,32 +139,38 @@ export default class CaseSelect extends React.Component {
                 {this.renderGroupHeader(group)}
             </tr>,
             <tr key={group + "row"}>
-                {this.renderGroup(group)}
+                {this.renderGroupCases(group)}
             </tr>
         ];
     }
 
     render() {
+        const topHeader = (
+            <tr><GroupHeader
+                key='allHeader'
+                onClick={() => this.toggleAll()}
+                colSpan='6'
+                name={'All Cases (57) | selected: ' + this.state.selected.length}
+            /></tr>
+            /* <tr><td
+                className='groupHeader'
+                onClick={() => this.toggleAll()}
+                colSpan='6'
+            >
+                
+            </td></tr> */
+        );
+
         let cases = [];
-
-        for (const pair of renderGroups["42"]) {
+        for (const pair of renderGroups["42"]) 
             cases = cases.concat(this.render42Group(pair));
-        }
-
-        for (const group of renderGroups["normal"]) {
+        for (const group of renderGroups["normal"])
             cases = cases.concat(this.renderNormalGroup(group));
-        }
 
         return (
             <div className="caseselect">
             <table><tbody>
-                <tr><td
-                    className='groupHeader'
-                    onClick={() => this.toggleAll()}
-                    colSpan='6'
-                >
-                    All Cases (57) | selected: {this.state.selected.length}
-                </td></tr>
+                {topHeader}
                 {cases}
             </tbody></table>
             </div>
