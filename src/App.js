ccd07a286720de0ce355f837bf8102a09cd120ea
Gabriel Shiu <gabriel@shiu.us>
2022-12-23 03:25:38 -0500

    most of the selection page, constants, organization

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import Timer from './Components/Timer/Timer.jsx';
+import CaseSelect from './Components/CaseSelect/CaseSelect.jsx';
 
 function App() {
   return (
     <div className="App">
-      <Timer />
+      <CaseSelect />
     </div>
   );
 }
